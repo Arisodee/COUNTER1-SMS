b775b9aabf5b6f85ff72e7156fb79fb3bef01649
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-11 17:42:58 +0300

    create activate  view function

--- counter1app/views.py
+++ counter1app/views.py
@@ -42,3 +42,18 @@ def signup(request):
      else:  
         form = SignUpForm()  
      return render(request, 'registration/signup.html', {'form': form})
+
+
+
+def activate(request, uidb64, token):  
+    try:  
+        uid = force_text(urlsafe_base64_decode(uidb64))  
+        user = User.objects.get(id=uid)  
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  
+        user = None  
+    if user is not None and account_activation_token.check_token(user, token):  
+        user.is_active = True  
+        user.save()  
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')  
+    else:  
+        return HttpResponse('Activation link is invalid!')
