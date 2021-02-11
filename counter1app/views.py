2829fe93f4c1e91d3448690b77a5fafa92ea3a82
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-11 17:41:08 +0300

    create signup view function

--- counter1app/views.py
+++ counter1app/views.py
@@ -12,3 +12,33 @@ from django.core.mail import EmailMessage
 
 
 # Create your views here.
+
+
+
+def signup(request):  
+    if request.method == 'GET':  
+        return render(request, 'accounts/signup.html')  
+    if request.method == 'POST':  
+        form = SignUpForm(request.POST)  
+        # print(form.errors.as_data())  
+        if form.is_valid():  
+            user = form.save(commit=False)  
+            user.is_active = False  
+            user.save()  
+            current_site = get_current_site(request)  
+            mail_subject = 'Activate your account.'  
+            message = render_to_string('registration/acc_active_email.html', {  
+                'user': user,  
+                'domain': current_site.domain,  
+                'uid': urlsafe_base64_encode(force_bytes(user.id)).decode(),  
+                'token': account_activation_token.make_token(user),  
+            })  
+            to_email = form.cleaned_data.get('email')  
+            email = EmailMessage(  
+                mail_subject, message, to=[to_email]  
+            )  
+            email.send()  
+            return HttpResponse('Please confirm your email address to complete the registration')  
+     else:  
+        form = SignUpForm()  
+     return render(request, 'registration/signup.html', {'form': form})
