8d2c8a1c575fad453edbb255a39dbc92c6718b5f
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-11 17:37:14 +0300

    import EmailMessage

--- counter1app/views.py
+++ counter1app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site  
+from django.utils.encoding import force_bytes, force_text  
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.template.loader import render_to_string  
+
+from .forms import SignUpForm  
+from .tokens import account_activation_token  
+from django.contrib.auth.models import User  
+from django.core.mail import EmailMessage
+
+
 # Create your views here.
