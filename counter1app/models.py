f69811760ed8d32106d6c20c98a7afb6f463333c
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-17 14:39:14 +0300

    add count class

--- counter1app/models.py
+++ counter1app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Count(models.Model):
+    name = models.CharField(max_length=80)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.name)
