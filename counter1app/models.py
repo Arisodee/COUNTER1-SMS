630b16c48e5cc832351844e15ecaeb5d7f6d1168
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-18 13:06:05 +0300

    add invitation class

--- counter1app/models.py
+++ counter1app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.mail import send_mail
+from django.template.loader import get_template
+from django.template import Context
+from django.conf import settings
 
 # Create your models here.
 class Count(models.Model):
@@ -7,3 +11,31 @@ class Count(models.Model):
 
     def __str__(self):
         return str(self.username)
+
+
+class Invitation(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    code = models.CharField(max_length=20)
+    sender = models.ForeignKey(User)
+
+  
+    def __unicode__(self):
+        return u'%s, %s' % (self.sender.username, self.email)
+
+    def send(self):
+        subject = u'Invitation to join Django Bookmarks'
+        link = 'http://%s/friend/accept/%s/' % (
+        settings.SITE_HOST,
+        self.code
+            )
+        template = get_template('invitation_email.txt')
+        context = Context({
+        'name': self.name,
+        'link': link,
+        'sender': self.sender.username,
+             })
+        message = template.render(context)
+        send_mail(
+        subject, message,
+        settings.DEFAULT_FROM_EMAIL, [self.email]    )    
