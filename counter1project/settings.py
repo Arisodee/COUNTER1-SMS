2a2c404c7d8e6a00ee1eaf84a877a0a39d668be3
Arisodee <arisodee@gmail.com>
2021-02-19 18:11:20 +0300

    fix errors

--- counter1project/settings.py
+++ counter1project/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'counter1project.wsgi.application'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sakoemmanuel4@gmail.com'
-EMAIL_HOST_PASSWORD = '0725939687'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') 
 
 
 
